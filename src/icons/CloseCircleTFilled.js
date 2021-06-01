90539a7281f1add656bdef602723d8d2b93fb38b
Jason <>
2021-06-01 14:41:07 +0800

    add icon

--- src/icons/CloseCircleTFilled.js
+++ src/icons/CloseCircleTFilled.js
@@ -11,7 +11,7 @@ const getIConNode = () => ({
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
         <circle fill="currentColor" cx={8} cy={8} r={8} />
         <path
-          className="close-circle-t-filled_svg__cls-1"
+          fill="inherit"
           d="M12.05 5.18l-1.21-1.21L8.01 6.8 5.18 3.97 3.97 5.18 6.8 8.01l-2.83 2.83 1.21 1.21 2.83-2.83 2.83 2.83 1.21-1.21-2.83-2.83 2.83-2.83z"
         />
       </svg>
