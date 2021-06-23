ea12b3ad96d7426d8733f5de6e333aa4c03c08b8
Jason <>
2021-06-23 10:48:47 +0800

    chore: release 1.0.17

--- src/icons/FacebookFilled.js
+++ src/icons/FacebookFilled.js
@@ -9,16 +9,16 @@ const getIConNode = () => ({
     };
     return (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
-        <g fill="currentColor">
-          <path d="M8 3.2C3.9 3.2 0 8 0 8s3.6 4.8 8 4.8S16 8 16 8c-4.2-5.2-8-4.8-8-4.8zm0 7.9c-1.7 0-3.1-1.4-3.1-3.1S6.3 4.9 8 4.9s3.1 1.4 3.1 3.1c0 1.7-1.4 3.1-3.1 3.1z" />
-          <circle cx={8} cy={8} r={1.9} />
-        </g>
+        <path
+          fill="currentColor"
+          d="M14.5 0h-13C.7 0 0 .7 0 1.5v13c0 .8.7 1.5 1.5 1.5H8V9H6V7h2V6c0-1.7 1.3-3 3-3h2v2h-2c-.6 0-1 .4-1 1v1h3l-.5 2H10v7h4.5c.8 0 1.5-.7 1.5-1.5v-13c0-.8-.7-1.5-1.5-1.5z"
+        />
       </svg>
     );
   },
 });
 
-const EyeOutlined = {
+const FacebookFilled = {
   functional: true,
 
   render(_, context) {
@@ -29,4 +29,4 @@ const EyeOutlined = {
     return <Icon {...filalyProps} component={getIConNode()} />;
   },
 };
-export default EyeOutlined;
+export default FacebookFilled;
